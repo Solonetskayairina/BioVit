84e56c3af165dbe89234536a9e3242016acaf0fa
Solonetskayairina <Solonetskayairina@ukr.net>
2024-04-27 19:15:04 +0300

    add files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+    import React from "react";
+    import {ROUTES} from "../../utils/routes";
+
+    const Header = () => {
+        return (
+            <div>
+                <div>
+                    <link to={ROUTES.HOME}>
+
+                    </link>
+                </div>
+            </div>
+        )
+    }
+
+    export default Header
